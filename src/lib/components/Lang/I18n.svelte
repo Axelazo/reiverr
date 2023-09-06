f54542738529ada589c42d15c520148e5129eced
Axel Aguilar <59715526+Axelazo@users.noreply.github.com>
2023-09-06 11:44:11 -0600

    feat: added logic to automatically register new Languages on the /src/lib/lang/
    All Languages within the /src/lib/lang/ will be automatically registered
    
    Import Messages function now allows to register languages that change between countries (like Portuguese) specifying the country after the name (Capitalized so i18n is able to register the Language) like: pt-BR.json (for Portuguese - Brazil) or pt-PT.json (for Portuguese - Portugal)

--- src/lib/components/Lang/I18n.svelte
+++ src/lib/components/Lang/I18n.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
 	import { settings } from '$lib/stores/settings.store';
 	import { addMessages, init, locale } from 'svelte-i18n';
-	import en from '../../lang/en.json';
-	import es from '../../lang/es.json';
-	import fr from '../../lang/fr.json';
-	import it from '../../lang/it.json';
 
-	addMessages('en', en);
-	addMessages('es', es);
-	addMessages('fr', fr);
-	addMessages('it', it);
+	interface LocaleDictionary {
+		[key: string]: LocaleDictionary | string | Array<string | LocaleDictionary> | null;
+	}
 
 	settings.subscribe((value) => {
 		if (value.language) {
@@ -18,6 +13,30 @@
 			locale.set('en');
 		}
 	});
+
+	const importMessages = async () => {
+		const messageFiles = import.meta.glob('../../lang/*.json');
+
+		for (const path in messageFiles) {
+			if (messageFiles.hasOwnProperty(path)) {
+				const fileName = path.match(/\/([^/]+)\.json$/)?.[1] || '';
+				const messages = await messageFiles[path]();
+
+				// Check if the filename contains a hyphen to detect language-country variations
+				if (fileName.includes('-')) {
+					// Use the full filename as the key (e.g., pt-br.json)
+					const language = fileName.split('.')[0];
+					addMessages(language, messages as LocaleDictionary);
+				} else {
+					// Use the language part only (e.g., es.json, en.json, it.json, fr.json)
+					const language = fileName.split('.')[0];
+					addMessages(language, messages as LocaleDictionary);
+				}
+			}
+		}
+	};
+
+	importMessages();
 
 	init({
 		initialLocale: $settings.language,
